a829dd893cc9920215ddf9edb27837a338e7bab9
yagiz <yagizkaya.personal@gmail.com>
2024-11-21 15:09:34 +0300

    Initial Commit

---
+++ TwitterCloneApp/TwitterCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneAppApp.swift
+//  TwitterCloneApp
+//
+//  Created by Yağız Kaya on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
